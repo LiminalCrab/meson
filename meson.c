b193223c4661e5e3d641306429313130814c317a
LiminalCrab <devpreston@protonmail.com>
2021-05-19 06:34:41 -0400

    updates

--- meson.c
+++ meson.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    long int uniqueId = 1387263000; // current time in miliseconds
-    long int tableId = 10000; //arbitrary
+    // All of these variables have arbitrary values, temporary example
+    long int uniqueId = 1387263000; // miliseconds
+    long int tableId = 10000; 
     int rowId = 5; // Row ID modulus 1024
-    int userId = 30000; // arbitrary
+    int userId = 30000; 
 
     long int x = uniqueId << (64 - 41);
     long int tbusMod = userId % tableId;
 
     x += tbusMod << (64 - 41 - 13);
     x += (rowId % 1024);
-    
+
     printf("UNIQUE ID: %ld \n", x);
     printf("BYTES: %ld", sizeof(x));
 
