35b558ff50bd48543b6b96e8de8f78b0dcdad3d6
LiminalCrab <devpreston@protonmail.com>
2021-06-06 05:33:25 -0400

    okay nm, setup generate_id function.

--- db_meta.c
+++ db_meta.c
@@ -1,65 +1,63 @@
 #include <stdio.h>
 #include <libpq-fe.h>
 
-int 
-db_connections()
-{
+int db_connections(){
     PGconn *conn;
 
     conn = PQconnectdb("");
 
-    switch(PQstatus(conn))
-    {
+    switch(PQstatus(conn)){
+
         case CONNECTION_STARTED:
-        printf("Waiting for connection to be made.");
+        printf("Waiting for connection to be made.\n");
         break;
 
         case CONNECTION_OK:
-        printf("Connect OK.");
+        printf("Connect OK.\n");
         break;
 
         case CONNECTION_NEEDED:
-        printf("Internal state: connect() needed.");
+        printf("Internal state: connect() needed.\n");
         break;
 
         case CONNECTION_CHECK_WRITABLE:
-        printf("Checking if session is read-write.");
+        printf("Checking if session is read-write.\n");
         break;
 
         case CONNECTION_CONSUME:
-        printf("Consuming any extra messages.");
+        printf("Consuming any extra messages.\n");
         break;
 
         case CONNECTION_GSS_STARTUP:
-        printf("Negotiating GSSAPI.");
+        printf("Negotiating GSSAPI.\n");
         break;
 
         case CONNECTION_CHECK_TARGET:
-        printf("Checking target server properties.");
+        printf("Checking target server properties.\n");
         break;
 
         case CONNECTION_MADE:
-        printf("Connection OK; waiting to send.");
+        printf("Connection OK; waiting to send.\n");
         break;
 
         case CONNECTION_BAD:
-        printf("Connection failed.");
+        printf("Connection failed.\n");
         break;
 
         case CONNECTION_AWAITING_RESPONSE:
-        printf("Waiting for a response from the server.");
+        printf("Waiting for a response from the server.\n");
         break;
 
         case CONNECTION_AUTH_OK:
-        printf("Received authentication; waiting for backend start-up to finish.");
+        printf("Received authentication; waiting for backend start-up to finish.\n");
         break;
 
         case CONNECTION_SSL_STARTUP:
-        printf("Negotiating SSL encryption.");
+        printf("Negotiating SSL encryption.\n");
         break;
 
         case CONNECTION_SETENV:
-        printf("Negotiating environment-driven parameter settings.");
+        printf("Negotiating environment-driven parameter settings.\n");
         break;
     }
 
--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 #include <libpq-fe.h>
 #include "sidgo.h"
 
-int 
-snowflake()
-{
-    // All of these variables have arbitrary values, temporary example
-    long int uniqueId = 1387263000; // miliseconds
-    long int tableId = 10000; 
-    int rowId = 5; // Row ID modulus 1024
-    int userId = 30000; 
+int generate_id(long int *uniqueId, long int *tableN, int *rowId, int *userId){
 
-    long int x = uniqueId << (64 - 41);
-    long int tbusMod = userId % tableId;
+
+
+    long int getUID = *uniqueId; // miliseconds, epoch from specific point in time.
+    long int getTblN = *tableN; // tablename 
+    int getRowId = *rowId; // Row ID modulus 1024
+    int getUsrId = *userId; 
+
+    long int x = getUID << (64 - 41);
+    long int tbusMod = getUsrId % getTblN;
 
     x += tbusMod << (64 - 41 - 13);
-    x += (rowId % 1024);
+    x += (getRowId % 1024);
 
     printf("UNIQUE ID: %ld \n", x);
     printf("BYTES: %ld", sizeof(x));
@@ -23,11 +23,19 @@ snowflake()
     return 0;
 }
 
+int query(void){
 
-int 
-main()
-{
+
+}
+
+int main(void){
    int x = db_connections();
+   if (x == CONNECTION_OK)
+   {
+
+    printf("Continuing...\n");
+
+   }
    printf("%d", x);
     return 0;
 }
