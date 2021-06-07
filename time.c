f4638af4bc6731c80bb7169502f064f960778944
LiminalCrab <devpreston@protonmail.com>
2021-06-07 07:24:48 -0400

    unncessary function removed, cleaned up comments, formatted code.

--- time.c
+++ time.c
@@ -3,50 +3,27 @@
 #include <sys/types.h>
 #include <time.h>
 #include <inttypes.h>
+
 #define DBIL  1000000000L
 
 
-long int gen_epoch(const long int lxArbEpochNs, long int cTimeNs){
-    /* 
-    So now we generate the epoch by converting time to miliseconds, and divide them by 1000000. 
-    Subtract the results from one another and return.
-    */
-    long int lxCTimeNs = cTimeNs;
-
-    const long int arbEpochMs = lxArbEpochNs / 1000000;
-    long int cTimeMs = lxCTimeNs / 1000000;
-
-    //ong int x = cTimeMs - arbEpochMs;
-    printf("CTIME: %ld, EPTIME: %ld\n", cTimeMs, arbEpochMs);
-    
-    return 0;
-}
-
 int epoch_data(void){
-    /* 
-    Possibly just use unix time, divide the difference from unix epoch to our given epoch,
-    I'll need the nanoseconds multiplier, to go to miliseconds. Ideally need to stay as long as
-    I can in ins struct timespec, only convert when I need to print / handle the final result.
-    Can't forget to normalize timespecs when calculating them.
-    */
 
     struct timespec ts;
-
-    /* Time since Saturday, January 1, 2011 12:00:00 AM in nanoseconds. */
-    const long int arbEpochNs = 1293840000000000000;
-    ts.tv_nsec = arbEpochNs; // output stEpoch
 
     /* Current time in nanoseconds. */
     long int ns;
     time_t sec;
-    uint64_t all;
+    uint64_t nsOut;
     clock_gettime(CLOCK_REALTIME, &ts);
     sec = ts.tv_sec;
     ns = ts.tv_nsec;
 
     /* final output in nano seconds. */
-    all = (uint64_t) sec * DBIL + (uint64_t) ns; 
-    gen_epoch(arbEpochNs, all);
+    nsOut = (uint64_t) sec * DBIL + (uint64_t) ns; 
 
-    return 0;
+    /* Convert to miliseconds */
+    long int cTimeMs = nsOut / 1000000;
+
+    return cTimeMs;
 }
